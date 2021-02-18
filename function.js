676a91a544d7efbf0426e21e5fc5f5b7253a5b0f
Florencia Wijaya <florenciawijaya11@gmail.com>
2021-02-18 23:33:12 +0700

    draw line

--- function.js
+++ function.js
@@ -1,11 +1,25 @@
 var gl;
+var program;
+const allShape = [];
 
-function initWebGL() {
+var sqButton = document.getElementById("sq-button");
+sqButton.addEventListener('click', function() {
+    drawAll("sq");
+})
+
+var lineButton = document.getElementById("line-button");
+lineButton.addEventListener('click', function() {
+    drawAll("line");
+})
+
+function start() {
     canvas = document.getElementById("draw-shape");
     gl = canvas.getContext("experimental-webgl");
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1, 1, 1, 1);
+    gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    program = createProg(gl, "vertex", "fragment");
 }
 
 function compileShader(gl, shader, shaderType) {
@@ -40,13 +54,10 @@ function initShader(gl, shaderId) {
     return compileShader(gl, shader.text, shaderType);
 }
 
-function initShaders(gl, vertexId, fragmentId) {
+function createProg(gl, vertexId, fragmentId) {
     let vertexShader = initShader(gl, vertexId);
     let fragmentShader = initShader(gl, fragmentId);
-    return createProg(gl, vertexShader, fragmentShader);
-}
 
-function createProg(gl, vertexShader, fragmentShader) {
     let program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
@@ -55,22 +66,46 @@ function createProg(gl, vertexShader, fragmentShader) {
     if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
         throw ("Failed to link " + gl.getProgramInfoLog(program));
     }
+
     return program;
 }
 
-function createRectangle() {
-    initWebGL();
-    program = initShaders(gl, "vertex", "fragment");
+function initBuffer(shape) {
+    if(shape == "sq") {
+        const verticesSq = new Float32Array([
+            -0.5, -0.5,
+            +0.5, -0.5,
+            +0.5, +0.5,
+            -0.5, +0.5
+        ]);
+
+        return verticesSq;
+    }
+    else if(shape == "line") {
+        const verticesLine = new Float32Array([
+            -0.5, 0.5,
+            0.5, 0.5
+        ]);
+        
+        return verticesLine;
+    }
+}
+
+function draw(shape) {
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
     let attributeLoc = gl.getAttribLocation(program, "aVertexPosition");
     let colorLoc = gl.getUniformLocation(program, "uColor");
-
-    const vertices = new Float32Array([
-        -1.0, -1.0,
-        +1.0, -1.0,
-        +1.0, +1.0,
-        -1.0, +1.0
+    let mat = gl.getUniformLocation(program, "formMatrix");
+    let identityMat = new Float32Array([
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
     ]);
     let itemSize = 2;
+
+    const vertices = initBuffer(shape);
 
     buff = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buff);
@@ -80,6 +115,56 @@ function createRectangle() {
     gl.enableVertexAttribArray(attributeLoc);
     gl.vertexAttribPointer(attributeLoc, itemSize, gl.FLOAT, false, 0, 0);
     gl.uniform4fv(colorLoc, [1, 0.0, 0.0, 1.0]);
+    gl.uniformMatrix4fv(mat, false, identityMat);
+    console.log(mat);
 
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    if(vertices.length == 8) { // square
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    }
+    else if(vertices.length == 4) { // line
+        gl.lineWidth(1.0);
+        gl.drawArrays(gl.LINES, 0, 2);
+    }
+}
+
+function drawAll(shape) {
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertices = initBuffer(shape);
+    let mat = gl.getUniformLocation(program, "formMatrix");
+    let identityMat = new Float32Array([
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ]);
+
+    gl.useProgram(program);
+    gl.uniformMatrix4fv(mat, false, identityMat)
+
+    const obj = [shape, vertices, identityMat];
+    allShape.push(obj);
+
+    for(let i = 0 ; i < allShape.length ; i++ ){ 
+        let attributeLoc = gl.getAttribLocation(program, "aVertexPosition");
+        let colorLoc = gl.getUniformLocation(program, "uColor");
+        let itemSize = 2;
+
+        buff = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buff);
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+        gl.enableVertexAttribArray(attributeLoc);
+        gl.vertexAttribPointer(attributeLoc, itemSize, gl.FLOAT, false, 0, 0);
+        gl.uniform4fv(colorLoc, [1, 0.0, 0.0, 1.0]);
+
+        if(vertices.length == 8) { // square
+            gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        }
+        else if(vertices.length == 4) { // line
+            gl.lineWidth(1.0);
+            gl.drawArrays(gl.LINES, 0, 2);
+        }
+    }
 }
